3a22d812f67cb9f88dae9529ee2b2e9eccf24517
Daryl Rogony <darylrogony@gmail.com>
2022-06-15 00:19:21 +0300

    commit tree d8f4998f4d636f27d1c7bd3a145cfe34ec0bc70c

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 8, "M", "white", "gentle",0);
+INSERT INTO bears VALUES (2,"Rowdy", 5, "F", "brown", "Protective", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 3, "M", "cinnamon", "empathetic", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 12, "F", "brown", "joyful", 1);
+INSERT INTO bears VALUES (5,"Melissa", 14, "M", "brown", "gentle", 0);
+INSERT INTO bears VALUES (6,"Grinch", 7, "M", "brown", "Social", 1);
+INSERT INTO bears VALUES (7,"Wendy", 7, "F", "black", "gentle", 0);
+INSERT INTO bears VALUES (8,NULL, 18, "M", "blond", "altruistic", 1);
+
--- lib/seed.sql
+++ lib/seed.sql
@@ -7,3 +7,12 @@ INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
 (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
 (7,"Wendy", 6, "F", "Blue", "naive", 1),
 (8,null, 20, "M", "black", "aggressive", 0);
+
+--  (1,"Mr. Chocolate", 8, "M", "white", "gentle", 0),
+--  (2,"Rowdy", 5, "F", "brown", "Protective",1),
+--  (3,"Tabitha", 3, "M", "cinnamon", "empathetic", 1),
+--  (4,"Sergeant Brown", 12, "F", "brown", "joyful", 1),
+--  (5,"Melissa", 14, "M", "brown", "gentle", 0),
+--  (6,"Grinch", 7, "M", "brown", "Social", 1),
+--  (7,"Wendy", 7, "F", "black", "gentle", 0),
+--  (8,NULL, 18, "M", "blond", "altruistic", 1);
