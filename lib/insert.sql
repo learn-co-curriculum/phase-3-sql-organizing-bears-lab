a4698905ecda4e5e72b2e361125b6317829b7b48
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-05-23 14:49:32 -0700

    sql_queries.rb

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 3, 'M', 'Black', 'Playful', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 6, 'F', 'Brown', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (
+        'Sergeant Brown',
+        8,
+        'M',
+        'Grizzly',
+        'Aggressive',
+        1
+    );
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 4, 'F', 'Polar', 'Curious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 10, 'M', 'Brown', 'Grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 7, 'F', 'Cinnamon', 'Energetic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 5, NULL, NULL, NULL, NULL)
